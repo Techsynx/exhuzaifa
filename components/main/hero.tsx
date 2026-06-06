0c0fa151d7aa6ea788da341da569e73a329a8335
Techsynx <techsynxx@gmail.com>
2026-06-06 18:46:30 +0500

    Update hero.tsx

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -9,8 +9,8 @@ export const Hero = () => (
       playsInline
       className="rotate-180 pointer-events-none absolute left-0 w-full object-cover -z-20
         top-[-200px] h-[calc(100%+200px)] object-[50%_100%]
-        md:top-[-250px] md:h-full md:object-center
-        max-md:top-[-80px] max-md:h-[60%]"
+        md:top-[-280px] md:h-full md:object-center
+        max-md:top-[-130px] max-md:h-[60%]"
     >
       <source src="/videos/blackhole.webm" type="video/webm" />
     </video>
