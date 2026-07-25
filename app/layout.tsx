4ce76585d6a813e223b42cc7df4dafeced7a7683
Techsynx <techsynxx@gmail.com>
2026-07-26 02:10:50 +0500

    Add Contra SDK script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 // import { Inter } from "next/font/google";
 import type { PropsWithChildren } from "react";
+import Script from "next/script";
 
 import { Navbar } from "@/components/main/navbar";
 import { Footer } from "@/components/main/footer";
@@ -31,6 +32,11 @@ export default function RootLayout({ children }: PropsWithChildren) {
         <Navbar />
         {children}
         <Footer />
+        <Script
+          src="https://contra.com/static/embed/sdk.js"
+          async
+          charset="utf-8"
+        />
       </body>
     </html>
   );
